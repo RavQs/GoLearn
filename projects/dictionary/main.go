9eb1afe61daea5ce74bd0473c595c344f2406af1
RavQs <ravenq1113@gmail.com>
2022-08-12 17:34:51 +0900

    Dictionary completed

--- projects/dictionary/main.go
+++ projects/dictionary/main.go
@@ -7,10 +7,21 @@ import (
 
 func main() {
 	dictionary := dict.Dictionary{"first": "firstNum"}
-	definition, err := dictionary.Search("first")
+
+	_, err := dictionary.Search("first")
+	err1 := dictionary.Add("first", "firstNum")
+	_, err2 := dictionary.Search("second")
+	err3 := dictionary.Add("second", "secondNum")
+	_, err4 := dictionary.Search("second")
+	err5 := dictionary.Update("second", "fourth")
+	fmt.Println(dictionary)
+	err6 := dictionary.Delete("first")
+	check(err, err1, err2, err3, err4, err5, err6)
+	fmt.Println(dictionary)
+}
+
+func check(err ...error) {
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
+		fmt.Println(err, ",")
 	}
 }
--- projects/dictionary/myDict/myDict.go
+++ projects/dictionary/myDict/myDict.go
@@ -5,6 +5,7 @@ import "errors"
 type Dictionary map[string]string
 
 var errNotFound = errors.New("Not found")
+var errAlreadyExist = errors.New("This word already exist")
 
 func (d Dictionary) Search(word string) (string, error) {
 	value, exists := d[word]
@@ -12,4 +13,40 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errAlreadyExist
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word, def string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case nil:
+		d[word] = def
+	case errNotFound:
+		return errNotFound
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case errNotFound:
+		return errNotFound
+	case nil:
+		delete(d, word)
+	}
+	return nil
 }
