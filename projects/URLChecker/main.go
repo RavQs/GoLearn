fb014f2f8ba7f009b9dfd56491ed791e7d5a66bb
RavQs <ravenq1113@gmail.com>
2022-08-13 14:02:41 +0900

    URL adresses added

---
+++ projects/URLChecker/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	urls := []string{
+		"https://nomadcoders.com/",
+		"https://www.google.com/",
+		"https://nastroyvse.ru/",
+		"https://quizlet.com/",
+		"https://app.milanote.com/",
+	}
+}
