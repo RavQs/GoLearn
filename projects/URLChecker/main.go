3f906dad140cef6d7ee24af0c1dbe8e5d58f3061
RavQs <ravenq1113@gmail.com>
2022-08-17 10:42:13 +0900

    urlChecker

--- projects/URLChecker/main.go
+++ projects/URLChecker/main.go
@@ -1,11 +1,33 @@
 package main
 
+import (
+	"errors"
+	"net/http"
+)
+
+var errRequest = errors.New("error")
+
 func main() {
 	urls := []string{
-		"https://nomadcoders.com/",
 		"https://www.google.com/",
 		"https://nastroyvse.ru/",
 		"https://quizlet.com/",
 		"https://app.milanote.com/",
+		"https://reddit.com/",
 	}
+
+	for _, url := range urls {
+		hitUrl(url)
+	}
+
+}
+
+func hitUrl(url string) error {
+	resp, err := http.Get(url)
+
+	if err == nil || resp.StatusCode >= 400 {
+		return errRequest
+	}
+
+	return nil
 }
