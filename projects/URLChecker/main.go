63463fcaa0d219f088269fce8c379eb200b60ec3
RavQs <ravenq1113@gmail.com>
2022-08-17 16:54:31 +0900

    Fixed URLCheckers(finish)

--- projects/URLChecker/main.go
+++ projects/URLChecker/main.go
@@ -9,14 +9,14 @@ import (
 var errRequest = errors.New("error")
 var errResults = make(map[string]string)
 
-type results struct {
+type resultRequest struct {
 	url    string
 	status string
 }
 
 func main() {
-
-	c := make(chan results)
+	results := make(map[string]string)
+	c := make(chan resultRequest)
 
 	urls := []string{
 		"https://www.google.com/",
@@ -32,16 +32,21 @@ func main() {
 	}
 
 	for i := 0; i < len(urls); i++ {
-		fmt.Println(<-c)
+		result := <-c
+		results[result.url] = result.status
+	}
+
+	for key, value := range results {
+		fmt.Println(key, value)
 	}
 
 }
 
-func hitUrl(url string, ch chan<- results) {
+func hitUrl(url string, ch chan<- resultRequest) {
 	resp, err := http.Get(url)
 	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
 		status = "FAILED"
 	}
-	ch <- results{url: url, status: status}
+	ch <- resultRequest{url: url, status: status}
 }
